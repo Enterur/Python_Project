5b8b05be9ae48c030430b671692d8580e97e1ea8
Kimmin_01 <kimmin12327@gmail.com>
2025-06-01 00:03:45 +0900

    threading을 통한 자연수 합 구현현

--- week11/OOP_h.w03.py
+++ week11/OOP_h.w03.py
@@ -1,19 +1,21 @@
 import threading
 
-class AddNumber:
+class AddNumber: # 자연수 합 class
     def __init__(self, number):
         self.number = number
 
-    def addNumber(self):
+    def addNumber(self): # 자연수 합 메서드
         total_sum = 0
         for i in range(1, self.number + 1):
             total_sum += i
         print(f"1 + 2 + 3 +....+  {self.number} = {total_sum}")
 
+# 서로 다른 범위의 인스턴스 생성
 sum1 = AddNumber(1000)
 sum2 = AddNumber(100000)
 sum3 = AddNumber(10000000)
 
+# .Thread를 통해 여러 개 thread 병렬 실행
 th1 = threading.Thread(target = sum1.addNumber)
 th2 = threading.Thread(target = sum2.addNumber)
 th3 = threading.Thread(target = sum3.addNumber)
